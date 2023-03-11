f3b941914320578acd9ddea666cecf4cabae5278
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-03-10 23:48:00 -0300

    Agrega estilos con bootstrap al resto de ejercicios
    Agrega bootstrap a los ejercicios de números, calculadora de salarios anual y mensual y datos del usuario.

--- tareas-clase-5/Ejercicio salario anual DOM/salario.js
+++ tareas-clase-5/Ejercicio salario anual DOM/salario.js
@@ -1,5 +1,3 @@
-console.log("hola")
-
 /*Cosas a tener en cuenta:
 1. Los <input> no tienen .innerText, en vez de eso, usan .value. 
 https://developer.mozilla.org/es/docs/Web/HTML/Elemento/input
@@ -17,24 +15,23 @@ al event handler “onclick”, agréguentle un return false; al final de la fun
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
-
 const $botonCalculoAnualMensual = document.querySelector('#boton-calcular')
 const $botonCalculoMensualAnual = document.querySelector('#boton-calcular-2')
 
-const MESES_EN_ANIO = 12
+const MESES_EN_ANIO = 12;
 
 $botonCalculoAnualMensual.onclick = function(){
-    let salarioAnual = Number(document.querySelector('#salario-anual').value)
-    let salarioMensual = salarioAnual / MESES_EN_ANIO
+    let salarioAnual = Number(document.querySelector('#salario-anual').value);
+    let salarioMensual = salarioAnual / MESES_EN_ANIO;
     
-    document.querySelector('#salario-mensual').value = salarioMensual
+    document.querySelector('#salario-mensual').value = salarioMensual;
     return false;
 }
 
 $botonCalculoMensualAnual.onclick = function(){
-    let salarioMensual = Number(document.querySelector('#salario-mensual-2').value)
-    let salarioAnual = salarioMensual * MESES_EN_ANIO
+    let salarioMensual = Number(document.querySelector('#salario-mensual-2').value);
+    let salarioAnual = salarioMensual * MESES_EN_ANIO;
 
-    document.querySelector('#salario-anual-2').value = salarioAnual
+    document.querySelector('#salario-anual-2').value = salarioAnual;
     return false;
 }
